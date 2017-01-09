7c41813c45c35aead7ed833b1c69fafe69afb33e
abraaoalves <abraao.teodosio@gmail.com>
2017-01-09 20:41:08 -0300

    fix(definition): expose interfaces

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,22 @@
 
-interface Config {
-  header: 'authorization';
-  cookie: 'feathers-jwt';
-  storageKey: 'feathers-jwt';
-  jwtStrategy: 'jwt';
-  path: '/authentication';
-  entity: 'user';
-  service: 'users';
+export as namespace authentication;
+
+export default e;
+
+declare function e(config?: e.Config) : () => void;
+
+declare namespace e {
+  const defaults: Config;
+
+  interface Config {
+    storage?:any;
+    header?: 'authorization';
+    cookie?: 'feathers-jwt';
+    storageKey?: 'feathers-jwt';
+    jwtStrategy?: 'jwt';
+    path?: '/authentication';
+    entity?: 'user';
+    service?: 'users';
+  }
 }
 
-export default function init(config?: Config) : () => void;
-export const defaults: Config;
